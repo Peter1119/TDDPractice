811a2d6087655ea32183b89db7a0b0beb3700eb7
nyokki1119 <ghdtjrgus333@naver.com>
2022-06-08 05:58:23 +0900

    chor. 프로젝트 생성

---
+++ PhotoApp/PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Sh Hong on 2022/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
