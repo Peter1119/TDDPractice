d33fcda1876d4d05cdf4f2c7d22151059d30fcd2
nyokki1119 <ghdtjrgus333@naver.com>
2022-06-10 06:10:21 +0900

    chor. 테스트 클래스 파일 추가하는 방법

--- PhotoApp/PhotoAppTests/PhotoAppTests.swift
+++ PhotoApp/PhotoAppTests/PhotoAppTests.swift
@@ -10,27 +10,34 @@ import XCTest
 
 class PhotoAppTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    static var classInstanceCounter = 0
+    
+    override class func setUp() {
+        super.setUp()
+        
+        print(#function, "class")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func setUp() {
+        PhotoAppTests.classInstanceCounter += 1
+        print(#function)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    override func tearDown() {
+        print(#function)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    override class func tearDown() {
+        super.tearDown()
+        print(#function, "class ")
     }
-
+    
+    func testExample1() {
+        print("Accessing class level informaion. Running from Instance # \(PhotoAppTests.classInstanceCounter)")
+    }
+    
+    func testExample2() {
+        print("Accessing class level informaion. Running from Instance # \(PhotoAppTests.classInstanceCounter)")
+    }
+    
 }
